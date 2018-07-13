6ab36d70c5acb86cde52739634a229f9a66bf39f
vaibhavbuccha <jainvaibhav415@gmail.com>
2018-07-13 13:41:10 +0530

    Add files via upload

---
+++ java script/basics/celsius.js
@@ -0,0 +1,21 @@
+var score = 100
+
+score = 130
+
+const bonus = 50
+
+var finalScore = (score + bonus) * 1.8
+
+var complex = ((4 + 4) * 5) / 2
+
+//console.log(finalScore)
+//console.log(complex)
+
+let tempInFahrenit = 32
+
+//do calculation
+//(T(F)-32) * 5 / 9
+
+let celsius = (tempInFahrenit-32) * 5 / 9
+
+console.log(celsius)
---
+++ java script/basics/variables.js
@@ -0,0 +1,20 @@
+//if we are not using semi colon thats will be totally fine because js is a ver intelligent language.
+let name='john' //string
+
+let score = 103 // number
+let bonus = 20
+let totalscore = score + bonus
+
+
+console.log(name)
+console.log(totalscore)
+
+// use camel caseing for making code more readable 
+let FirstName = 'john '
+//override the name ..
+FirstName = 'jane'
+//if you uses the const avriable so you can't over ride because it is the rigid variable
+let lastName = 'doe'
+let fullName = FirstName+'** '+lastName
+console.log(FirstName+lastName)
+console.log(fullName);
